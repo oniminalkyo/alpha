e9c2aeb57332783a20611784e293d2e204fee737
paul <paul.example.com>
2018-12-30 23:29:53 +0800

    add strategy

---
+++ config/configuration.py
@@ -0,0 +1,9 @@
+class Configuration(object):
+    BASIC_DATA_DIR = "../data_source/basic_data/"
+    TRANSACTION_DATA_DIR = "../data_source/transaction_data/"
+
+    CODE = "股票代码"
+    NAME = "股票名称"
+    AREA = "公司所在地"
+    CLASS = "股票类别"
+    INDUSTRY = "所属行业"
